ce6fbe663e34a814dcc63d74522635daee188a97
Mark Wolfman <canismarko@gmail.com>
2018-07-09 17:14:42 -0500

    Added track-specific volumes and fade-times. Also, trimmed silence from start of fanfare.

--- dndmusic.py
+++ dndmusic.py
@@ -14,29 +14,31 @@ import vlc
 MUSIC_DIR = os.path.abspath(os.path.dirname(__file__))
 # MUSIC_DIR = '/home/mwolf/src/dndmusic/'
 
+FADE_TIME = 1.5
 
 class Player():
+    # List of key assignments: (song_file, volume, fade_time)
     key_assignments = {
-        keyboard.KeyCode.from_char('0'): os.path.join(MUSIC_DIR, 'battle_music_1.mp3'),
-        keyboard.KeyCode.from_char('000'): os.path.join(MUSIC_DIR, 'battle_music_2.mp3'),
-        keyboard.KeyCode.from_char('.'): os.path.join(MUSIC_DIR, 'battle_music_3.mp3'),
-        keyboard.KeyCode.from_char(','): os.path.join(MUSIC_DIR, 'battle_music_3.mp3'),
-        keyboard.KeyCode.from_char('1'): os.path.join(MUSIC_DIR, 'forest_sounds_1.mp3'),
-        keyboard.KeyCode.from_char('2'): os.path.join(MUSIC_DIR, 'town_sounds_1.mp3'),
-        keyboard.KeyCode.from_char('3'): os.path.join(MUSIC_DIR, 'tavern_sounds_1.mp3'),
-        keyboard.KeyCode.from_char('4'): os.path.join(MUSIC_DIR, 'cave_sounds_1.m4a'),
-        keyboard.KeyCode.from_char('5'): os.path.join(MUSIC_DIR, 'crowded_bar_1.opus'),
-        keyboard.KeyCode.from_vk(65437): None,
-        keyboard.KeyCode.from_char('6'): os.path.join(MUSIC_DIR, 'orc_grunts.m4a'),
-        keyboard.KeyCode.from_char('7'): os.path.join(MUSIC_DIR, 'holst_neptune.opus'),
-        keyboard.KeyCode.from_char('8'): os.path.join(MUSIC_DIR, 'holst_saturn.opus'),
-        keyboard.KeyCode.from_char('9'): os.path.join(MUSIC_DIR, 'holst_mars.ogg'),
-        keyboard.Key.enter: 'Stop',
-        keyboard.Key.backspace: 'Pause',
-        keyboard.KeyCode.from_char('-'): 'VolDown',
-        keyboard.KeyCode.from_char('+'): 'VolUp',
-        keyboard.KeyCode.from_char('*'): os.path.join(MUSIC_DIR, 'victory_fanfare.m4a'),
-        keyboard.KeyCode.from_char('/'): None,
+        keyboard.KeyCode.from_char('0'): ('battle_music_1.mp3', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('000'): ('battle_music_2.mp3', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('.'): ('battle_music_3.mp3', 100, FADE_TIME),
+        keyboard.KeyCode.from_char(','): ('battle_music_3.mp3', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('1'): ('forest_sounds_1.mp3', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('2'): ('town_sounds_1.mp3', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('3'): ('tavern_sounds_1.mp3', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('4'): ('cave_sounds_1.m4a', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('5'): ('crowded_bar_1.opus', 100, FADE_TIME),
+        keyboard.KeyCode.from_vk(65437): ('crowded_bar_1.opus', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('6'): ('orc_grunts.m4a', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('7'): ('holst_neptune.opus', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('8'): ('holst_saturn.opus', 100, FADE_TIME),
+        keyboard.KeyCode.from_char('9'): ('holst_mars.ogg', 100, FADE_TIME),
+        keyboard.Key.enter: ('Stop', None, None),
+        keyboard.Key.backspace: ('Pause', None, None),
+        keyboard.KeyCode.from_char('-'): ('VolDown', None, None),
+        keyboard.KeyCode.from_char('+'): ('VolUp', None, None),
+        keyboard.KeyCode.from_char('*'): ('victory_fanfare.m4a', 100, 0.2),
+        keyboard.KeyCode.from_char('/'): (None, None, None),
     }
     _player = None
     volume = 100
@@ -47,16 +49,16 @@ class Player():
     def __init__(self):
         self.instance = vlc.Instance('--input-repeat=999999', '--quiet')
    
-    def stop_music(self):
+    def stop_music(self, fade_time=FADE_TIME):
         if self._player is not None:
             log.debug('Stopping music')
-            self.fade_volume(0)
+            self.fade_volume(0, fade_time=fade_time)
             self._player.stop()
             # Remove the VLC media player
             del self._player
             self._player = None
     
-    def fade_volume(self, target, fade_time=1.5):
+    def fade_volume(self, target, fade_time=FADE_TIME):
         if self._player is not None:
             init_vol = self._player.audio_get_volume()
             curr_vol = init_vol
@@ -64,18 +66,19 @@ class Player():
             # Fade in the volume
             for i in range(self.fade_steps):
                 curr_vol = curr_vol + delta_vol/self.fade_steps
+                log.debug("Volume set to %d", curr_vol)
                 self._player.audio_set_volume(round(curr_vol))
                 time.sleep(fade_time / self.fade_steps)
             log.debug('Faded volume from %d to %d', init_vol, round(curr_vol))
     
-    def start_music(self, song_file):
+    def start_music(self, song_file, fade_time):
         if os.path.exists(song_file):
             log.info("Starting song: %s", song_file)
             self._player = self.instance.media_player_new(f'file://{song_file}')
             self._player.audio_set_volume(0)
             self._player.play()
-            time.sleep(0.1)
-            self.fade_volume(self.volume)
+            time.sleep(0.01)
+            self.fade_volume(self.volume, fade_time=fade_time)
         else:
             log.error('Song file not found: %s', song_file)
     
@@ -86,9 +89,9 @@ class Player():
     
     def on_press(self, key):
         log.debug("Pressed key %s", key)
-        action = self.key_assignments.get(key, None)
+        (action, vol, fade_time) = self.key_assignments.get(key, (None, None, None))
         if action == "Stop":
-            self.stop_music()
+            self.stop_music(fade_time=FADE_TIME)
         elif action == 'VolUp':
             self.change_volume(10)
         elif action == 'VolDown':
@@ -96,8 +99,10 @@ class Player():
         elif action == 'Pause':
             self.toggle_pause()
         elif action is not None:
-            self.stop_music()
-            self.start_music(action)
+            self.volume = vol
+            song_file = os.path.join(MUSIC_DIR, action)
+            self.stop_music(fade_time=fade_time)
+            self.start_music(song_file, fade_time=fade_time)
     
     def change_volume(self, delta_vol):
         new_vol = self.volume + delta_vol
@@ -112,6 +117,7 @@ class Player():
             log.debug("Changed volume from %d to %d", old_vol, new_vol)
         else:
             log.info("Volume NOT changed from %d to %d", old_vol, new_vol)
+
 
 def monitor_keypresses():
     player = Player()
